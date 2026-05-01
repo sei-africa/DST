62e49806e57901c41840f7623b51f33e7601aa57
SEI-AFRICA <rija.faniriantsoa@sei.org>
2026-05-01 13:10:09 +0300

    update

--- app/scripts/_global.py
+++ app/scripts/_global.py
@@ -9,8 +9,14 @@ GLOBAL_CONFIG['app_dir'] = app_dir
 
 GLOBAL_CONFIG['data_dir'] = config.DST_DATA_DIR
 GLOBAL_CONFIG['app_cache'] = os.path.join(config.DST_DATA_DIR, 'app_cache')
+if not os.path.exists(GLOBAL_CONFIG['app_cache']):
+    os.makedirs(GLOBAL_CONFIG['app_cache'])
 GLOBAL_CONFIG['users_data'] = os.path.join(config.DST_DATA_DIR, 'users_data')
+if not os.path.exists(GLOBAL_CONFIG['users_data']):
+    os.makedirs(GLOBAL_CONFIG['users_data'])
 GLOBAL_CONFIG['shp_dir'] = os.path.join(config.DST_DATA_DIR, 'shp')
+if not os.path.exists(GLOBAL_CONFIG['shp_dir']):
+    os.makedirs(GLOBAL_CONFIG['shp_dir'])
 
 met_file = os.path.join(app_dir, 'yaml', 'metService.yaml')
 GLOBAL_CONFIG['metInfo'] = load_yaml_file(met_file)
